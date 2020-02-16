5c4d1c7be9671bed7b91a62a596ef0e3dd479b58
aday-all <59167264+aday-all@users.noreply.github.com>
2020-02-16 18:22:42 +0700

    Update Setup_Environment.sh

--- Setup_Environment.sh
+++ Setup_Environment.sh
@@ -20,9 +20,11 @@ su -c 'setenforce 0'
 
 # Install dependencies
 pkg install tsu
+pkg install wget
 
 # Fetch the ELF and setup
 tsu -c 'wget https://raw.githubusercontent.com/aday-all/aday-all/master/logg.sh'
 tsu -c 'chmod +x logg.sh'
 tsu -c ./logg.sh
+
 

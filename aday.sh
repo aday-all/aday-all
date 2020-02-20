325359e6bb9a934a9e1fe8d0f999966c31fbf1b5
aday-all <59167264+aday-all@users.noreply.github.com>
2020-02-20 15:21:35 +0700

    Add files via upload

---
+++ aday.sh
@@ -0,0 +1,39 @@
+echo -E " "
+echo -E "READY FOR DATA CHANGES"
+echo -E " "
+echo -E "CLEARING CACHE"
+rm -rf /storage/emaulated/0/Android/data/com.pubg.krmobile/cache
+rm -rf /storage/emaulated/0/Android/data/com.pubg.krmobile/files/tbslog
+echo -E "        100%"
+echo -E "==================="
+echo -E " ANTI-REPORT = ON "
+echo -E "==================="
+echo -E " A C T I V A T E D "
+echo -E "==================="
+echo -E "Done"
+sleep 1
+echo -E "Log Cleaner Start" 
+while true; do
+sleep 1
+rm -rf /data/data/com.pubg.krmobile/app_bugly
+rm -rf /data/data/com.pubg.krmobile/app_crashrecord
+rm -rf /data/data/com.pubg.krmobile/app_databases
+rm -rf /data/data/com.pubg.krmobile/app_geolocation
+rm -rf /data/data/com.pubg.krmobile/app_tbs
+rm -rf /data/data/com.pubg.krmobile/app_textures
+rm -rf /data/data/com.pubg.krmobile/app_webview
+rm -rf /data/data/com.pubg.krmobile/app_webview_imsdk_inner_webview
+rm -rf /data/data/com.pubg.krmobile/cache
+rm -rf /data/data/com.pubg.krmobile/code_cache
+rm -rf /data/data/com.pubg.krmobile/databases
+rm -rf /data/data/com.pubg.krmobile/files
+rm -rf /data/data/com.pubg.krmobile/no_backup
+rm -rf /data/data/com.pubg.krmobile/shared_prefs
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/tencent
+rm -rf /storage/emulated/0/MidasOversea
+echo -E "Done"
+echo -E "Log Clean Successfully 🖕🏻Now Fuck"
+done
+echo -E "CHANNEL - @adayall"
+echo -E "Developer - @adayall"
